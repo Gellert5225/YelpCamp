49ccc705eaba36b23deee1fb60c4102f6692e206
Gellert <gellert.li1995@gmail.com>
2016-10-16 17:02:44 +0800

    bugs

--- models/campground.js
+++ models/campground.js
@@ -27,4 +27,4 @@ var campgroundSchema = new mongoose.Schema({
 //    );
 //});
 
-module.exports = mongoose.model("Campground", campgroundSchema);obje
+module.exports = mongoose.model("Campground", campgroundSchema);
