f770085752e2c7866d06470c1e6a94af80bded2c
Gellert <gellert.li1995@gmail.com>
2016-10-16 17:36:43 +0800

    strategy

--- app.js
+++ app.js
@@ -49,8 +49,27 @@ app.use(indexRoutes);
 app.use(commentRoutes);
 app.use(campgroundsRoutes);
 
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.use(new LocalStrategy(function(username, password, done) {
+        User.findOne({ username: username }, function (err, user) {
+            if (err) { return done(err); }
+            if (!user) {
+                return done(null, false, { message: 'Incorrect username.' });
+            }
+            if (!user.validPassword(password)) {
+                return done(null, false, { message: 'Incorrect password.' });
+            }
+            return done(null, user);
+        });
+    }
+));
+passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+            done(err, user);
+        });
+    });
 
 app.listen(process.env.PORT || 4000);
